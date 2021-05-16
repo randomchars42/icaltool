06559348c9763dbb33ad942675b9ca55a54e33d3
randomchars42 <randomchars42@users.noreply.github.com>
2021-05-16 22:25:47 +0200

    remove file that cannot work

--- activate_venv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# approach taken from:
-# https://codefather.tech/blog/bash-get-script-directory/
-script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-source "$script_dir/venv/bin/activate"
-echo "type 'deactivate' to deactivate (read more in 'note.md')"
