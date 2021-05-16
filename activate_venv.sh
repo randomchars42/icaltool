6425ae109dfd743ad2ab01f45e3391ce4bba740e
randomchars42 <randomchars42@users.noreply.github.com>
2021-05-16 22:16:28 +0200

    fix error in script activating venv

--- activate_venv.sh
+++ activate_venv.sh
@@ -3,5 +3,5 @@
 # approach taken from:
 # https://codefather.tech/blog/bash-get-script-directory/
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-source "$script_dir/venv/icaltool/bin/activate"
+source "$script_dir/venv/bin/activate"
 echo "type 'deactivate' to deactivate (read more in 'note.md')"
