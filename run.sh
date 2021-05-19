eaa88638bfb56d2b14c3a7a790fcb99b32c35f94
randomchars42 <randomchars42@users.noreply.github.com>
2021-05-19 21:33:49 +0200

    make run.sh more useful (pass all params on)

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
-cd ./src
-python3 -m icaltool.icaltool
+#!/usr/bin/env bash
+
+# approach taken from:
+# https://codefather.tech/blog/bash-get-script-directory/
+script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+current_dir=$PWD
+# go back to the current directory even if the script is interrupted
+trap "cd $PWD" EXIT
+
+cd "$script_dir/src"
+python3 -m icaltool.icaltool "$@"
+cd $PWD
